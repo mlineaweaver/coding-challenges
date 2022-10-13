cfc209123b7c88e33d3488aefa0b3e4b64249e52
Joshua Lineaweaver <jlineaweaver@Joshuas-MacBook-Air.local>
2022-10-13 14:04:55 -0600

    First challenge

---
+++ array-or-dict.js
@@ -0,0 +1,13 @@
+
+student = {name:"Josh", score:"99"};
+student2 = {name:"Rick", score:"64"};
+
+students = [student, student2];
+
+// Which student had the highest score?
+function highestScore(students) {
+
+  console.log("unknown");
+}
+
+highestScore(students);
