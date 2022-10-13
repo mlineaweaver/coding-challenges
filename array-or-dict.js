20be9a420e9e5c37bcdce069b8e27103a3ff9562
Melissa Lineaweaver <MelissaHastie16@gmail.com>
2022-10-13 14:25:32 -0600

    solving an array problem

--- array-or-dict.js
+++ array-or-dict.js
@@ -1,13 +1,21 @@
-
-student = {name:"Josh", score:"99"};
-student2 = {name:"Rick", score:"64"};
+student = { name: "Josh", score: "99" };
+student2 = { name: "Rick", score: "64" };
 
 students = [student, student2];
 
 // Which student had the highest score?
 function highestScore(students) {
+  hs = 0;
+  person = "";
+  for (i = 0; i < students.length; i++) {
+    student = students[i];
+    if (student["score"] > hs) {
+      hs = student["score"];
+      person = student["name"];
+    }
+  }
 
-  console.log("unknown");
+  console.log(person + " " + hs);
 }
 
 highestScore(students);
